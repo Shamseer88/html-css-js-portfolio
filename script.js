0facc9ea707326e0beb10d6fc9fd7ccb7934776f
alishamseer7 <alishamseer@gmail.com>
2023-07-17 20:14:37 +0530

    portfolio section started

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const scrollLinks = document.querySelectorAll('.scroll-link');
-    
-    scrollLinks.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      });
-    });
-  });
+const portfolios = [
+  {
+    id: 1,
+    title: 'My Portfolio',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This project is a visually stunning portfolio website showcasing my work and skills. Built using HTML, CSS, and JavaScript, it provides an interactive and engaging experience for visitors.',
+    git: 'https://github.com/Shamseer88/html-css-js-portfolio',
+    websiteLink: 'https://shamseer88.github.io/html-css-js-portfolio/',
+    image: 'images/my-portfolio.png'
+  },
+  {
+    id: 2,
+    title: 'Counter App',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This app, developed using HTML, CSS, and JavaScript, features interactive buttons that increment, decrement, and reset a displayed number. It provides a user-friendly interface for manipulating values with ease.',
+    git: 'https://github.com/Shamseer88/counter-javascript',
+    websiteLink: 'https://shamseer88.github.io/counter-javascript/',
+    image: 'images/counter-app.png'
+  },
+  {
+    id: 3,
+    title: 'My Portfolio',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This project is a visually stunning portfolio website showcasing my work and skills. Built using HTML, CSS, and JavaScript, it provides an interactive and engaging experience for visitors.',
+    git: 'https://github.com/Shamseer88/html-css-js-portfolio',
+    websiteLink: 'https://shamseer88.github.io/html-css-js-portfolio/',
+    image: 'images/my-portfolio.png'
+  },
+  {
+    id: 4,
+    title: 'Counter App',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This app, developed using HTML, CSS, and JavaScript, features interactive buttons that increment, decrement, and reset a displayed number. It provides a user-friendly interface for manipulating values with ease.',
+    git: 'https://github.com/Shamseer88/counter-javascript',
+    websiteLink: 'https://shamseer88.github.io/counter-javascript/',
+    image: 'images/counter-app.png'
+  },
+  {
+    id: 5,
+    title: 'My Portfolio',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This project is a visually stunning portfolio website showcasing my work and skills. Built using HTML, CSS, and JavaScript, it provides an interactive and engaging experience for visitors.',
+    git: 'https://github.com/Shamseer88/html-css-js-portfolio',
+    websiteLink: 'https://shamseer88.github.io/html-css-js-portfolio/',
+    image: 'images/my-portfolio.png'
+  },
+  {
+    id: 6,
+    title: 'Counter App',
+    tech: 'HTML, CSS, JavaScript',
+    description: 'This app, developed using HTML, CSS, and JavaScript, features interactive buttons that increment, decrement, and reset a displayed number. It provides a user-friendly interface for manipulating values with ease.',
+    git: 'https://github.com/Shamseer88/counter-javascript',
+    websiteLink: 'https://shamseer88.github.io/counter-javascript/',
+    image: 'images/counter-app.png'
+  }
+]
