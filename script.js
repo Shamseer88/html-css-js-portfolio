3f2d60c44dd4eb99bbda5977d3e9a18f65aa068a
alishamseer7 <alishamseer@gmail.com>
2023-07-18 07:48:22 +0530

    portfolio section desktop finished

--- script.js
+++ script.js
@@ -54,3 +54,61 @@ const portfolios = [
     image: 'images/counter-app.png'
   }
 ]
+
+const cards = document.querySelector('.portfolio-cards')
+portfolios.forEach(function(portfolio) {
+  window.addEventListener('DOMContentLoaded', function() {
+    const card = document.createElement('div');
+    card.className = 'card portfolio-card';
+    cards.appendChild(card);
+    
+    const cardImg = document.createElement('img');
+    cardImg.className = 'card-img-top';
+    cardImg.src = portfolio.image;
+    card.appendChild(cardImg);
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    card.appendChild(cardBody);
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.className = 'card-title portfolio-card-title';
+    cardTitle.innerText = portfolio.title;
+    cardBody.appendChild(cardTitle);
+
+    const cardText = document.createElement('p');
+    cardText.className = 'card-text portfolio-card-text';
+    cardText.innerText = 'Technology used: '+portfolio.tech;
+    cardBody.appendChild(cardText);
+
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    card.appendChild(overlay);
+
+    const overlayText = document.createElement('div');
+    overlayText.className = 'text';
+    overlay.appendChild(overlayText);
+
+    const overlayDescription = document.createElement('p');
+    overlayDescription.innerText = portfolio.description;
+    overlayText.appendChild(overlayDescription);
+
+    const githubLink = document.createElement('a');
+    githubLink.className = 'btn btn-primary link-btn';
+    githubLink.href = portfolio.git;
+    githubLink.target = '_blank';
+    overlayText.appendChild(githubLink);
+    const githubIcon = document.createElement('i');
+    githubIcon.className = 'fa-brands fa-github';
+    githubLink.appendChild(githubIcon);
+
+    const websiteLink = document.createElement('a');
+    websiteLink.className = 'btn btn-primary link-btn';
+    websiteLink.href = portfolio.websiteLink;
+    websiteLink.target = '_blank';
+    overlayText.appendChild(websiteLink);
+    const websiteIcon = document.createElement('i');
+    websiteIcon.className = 'fa-solid fa-link';
+    websiteLink.appendChild(websiteIcon);
+  })
+})
